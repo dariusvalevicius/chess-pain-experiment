a39e18c6f58d941ab824706122f17d8bf6fd5663
Jérôme Genzling <111453682+JGenzling@users.noreply.github.com>
2022-11-28 17:01:04 -0500

    Update Inputs
    Change the flow zone (-150,-50) instead of (-50,+50)
    Added the easy puzzles df

--- Generate_inputs.py
+++ Generate_inputs.py
@@ -4,10 +4,11 @@ import random
 
 elo_player = int(input('Please enter the elo of the player: \n'))
 
-min_flow_elo, max_flow_elo = elo_player-50,elo_player+50
+min_flow_elo, max_flow_elo = elo_player-150,elo_player-50
 min_hard_elo, max_hard_elo = elo_player+550,elo_player+650
 
 puzzles = pd.read_csv('subset_lichess.csv')
+easy_puzzles = pd.read_csv('Final_Easy_set.csv')
 
 subset_flow = puzzles[(puzzles['Rating'] < max_flow_elo) & (puzzles['Rating'] > min_flow_elo)]
 subset_flow_clean = subset_flow[['PuzzleId', 'FEN', 'Moves', 'Rating']]
@@ -24,5 +25,6 @@ def pick_a_puzzle(subset):
     subset = subset.drop(subset.index[[pick-1]])
     print(pick)
     return puzzle,subset
+    
 
 
